bc329d0822517a28f3778ea71f6f048d22acc344
SwethaKoppuravuri <119507872+SwethaKoppuravuri@users.noreply.github.com>
2023-01-23 07:27:25 +0530

    check relational operators

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 7, b = 11;
+
+        System.out.println("a is " + a + " and b is " + b);
+
+        // == operator
+        System.out.println(a == b);
+
+        // != operator
+        System.out.println(a != b);
+
+        // > operator
+        System.out.println(a > b);
+
+        // < operator
+        System.out.println(a < b);
+
+        // >= operator
+        System.out.println(a >= b);  // false
+
+        // <= operator
+        System.out.println(a <= b);  // true
+    }
+}
